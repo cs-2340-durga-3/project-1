f1ebc74908995b19a51e902cb97531d4603382ab
cubedhuang <99danielh@gmail.com>
2025-02-14 15:26:37 -0500

    Add persisted sqlite mount

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'moviesstore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3' if FLY_APP_NAME is None else '/mnt/gtmovies_db/gtmovies_db.sqlite',
     }
 }
 
